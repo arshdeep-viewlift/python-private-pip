ba4257fec43fee976ceed3c53c95ccd0b93f8d98
arshkharbanda2010 <arsh.kharbanda2010@gmail.com>
2022-10-25 14:08:45 +0530

    files

---
+++ python-private-pip/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='python-private-pip',
+    packages=['python-private-pip'],
+    description='Hello world!!',
+    version='1.0',
+    url='https://github.com/arshdeep-viewlift/python-private-pip',
+    author='viewlift',
+    keywords=['pip','python','private']
+    )
